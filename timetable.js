36d17ac2f3e070f83557fd93091c1e4af620765a
Kevin Shi <k.shi@me.com>
2015-07-26 16:06:02 +1000

    Big code refactoring: moved code into 'libraries'

--- timetable.js
+++ timetable.js
@@ -1,98 +1,13 @@
 var Activities = new Mongo.Collection("activities");
 if (Meteor.isClient) {
-  Session.set('colorKey', '');
-  // 'relative times' toggle
-  Template.relativeTimesPicker.helpers({
-    naturalDisplay: function () { return Session.get("timeSet"); }
-  })
 
-  Template.relativeTimesPicker.events({
-    "change .naturalDisplay input": function (event) {
-      Session.set("timeSet", event.target.checked);
-    }
-  }),
-  
-  Template.relativeTimesPicker.helpers({
-  	futureOnly: function() { return Session.get("nowSet"); }
-  })
-  
-  Template.futureOnlyPicker.created = function() {
-  	this.filter = new ReactiveTable.Filter('future', ['Time']);
-  };
-  
-  Template.futureOnlyPicker.events({
-    "change .futureOnly input": function (event, template) {
-      Session.set("nowSet", event.target.checked);
-      if (event.target.checked) {
-      	template.filter.set({'$gte': new Date()});
-      } else {
-      	template.filter.set('');
-      }
-    }
-  }),
-   
+  // hide the help alert displayed on startup
   Template.body.events({
     "click": function (event) {
       $("div.alert").alert('close');
     }
   }),
-  
-  Template.colorPicker.events({
-    "click .btn-default": function (event, template) {
-      var colorPicked = function () {
-      	switch (event.target.innerText) {
-      		case 'Activity type': return 'Activity';
-      		case 'Theme': return 'Theme';
-      		default: return '';
- 	   	}
-      };
-      Session.set('colorKey', colorPicked());
-    }
-  }),
 
-  Template.activityFilter.created = function () {
-    this.filter = new ReactiveTable.Filter('activity', ['Activity']);
-  };
-  Template.activityFilter.events({
-  	"click .btn-default": function (event, template) {
-      template.filter.set(event.target.innerText.slice(0, -1));
-  		if (event.target.innerText === "All") {
-  			template.filter.set('');
-  		}
-  	}
-  });
-  
-  Template.themeFilter.created = function () {
-    this.filter = new ReactiveTable.Filter('theme', ['Theme']);
-  };
-  Template.themeFilter.events({
-  	"click .btn-default": function (event, template) {
-      template.filter.set(event.target.innerText);
-  		if (event.target.innerText === "All") {
-  			template.filter.set('');
-  		}
-  	}
-  });
-  // name filter
-  Template.nameFilter.created = function () {
-    this.filter = new ReactiveTable.Filter('search', ['Description']);
-  };
-
-  Template.nameFilter.events({
-     "keyup .reactive-table-input, input .reactive-table-input": function (event, template) {
-       template.filter.set(event.target.value);
-     } 
-  }); 
-  
-  // discipline filter
-  Template.disciplineFilter.created = function () {
-    this.filter = new ReactiveTable.Filter('discipline', ['Discipline', 'Subdiscipline']);
-  };
-  Template.disciplineFilter.events({
-     "keyup .reactive-table-input, input .reactive-table-input": function (event, template) {
-       template.filter.set(event.target.value);
-     } 
-  });
   Template.timetable.helpers({
     settings: function () {
       return {
@@ -102,26 +17,7 @@ if (Meteor.isClient) {
         showRowCount: true,
         showNavigation: 'true',
         showColumnToggles: false,
-        rowClass: function(item) {
-          if (Session.get('colorKey') === 'Activity') {
-            switch (item.Activity.split("_")[0]) {
-              case 'Lecture': return 'info';
-              case 'Practical': return 'danger';
-              case 'Tutorial': return 'success';
-              default: return '';
-            }
-          } else if (Session.get('colorKey') === 'Theme') {
-             switch (parseInt(item.Theme)) {
-              case 1: return 'danger';
-              case 2: return 'warning';
-              case 3: return 'info';
-              case 4: return 'success';
-              default: return '';
-            }
-          } else {
-            return '';
-          }
-        },
+        rowClass: colorFunction,
         filters: ['activity', 'theme', 'discipline', 'search', 'future'],
         fields: [
           { key: 'Activity', label: 'Activity', fn: function(val, obj) {
