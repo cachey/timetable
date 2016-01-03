e3a80430652b2908798737f185ea9e74f93b895f
Kevin Shi <k.shi@me.com>
2016-01-03 22:16:09 +1100

    Code cleanup and formatting

--- timetable.js
+++ timetable.js
@@ -1,7 +1,6 @@
 var Units = new Meteor.Collection('units');
 var Activities = new Meteor.Collection('activities');
 
-
 Router.configure({
   layoutTemplate: 'main'
 });
@@ -14,7 +13,9 @@ Router.route('/view/:unit', {
   template: 'display',
   data: function() {
     Session.set('unit', parseInt(this.params.unit));
-    var unitInfo = Units.findOne({number: parseInt(this.params.unit)});
+    var unitInfo = Units.findOne({
+      number: parseInt(this.params.unit)
+    });
     Session.set('halfway', unitInfo.halfway);
     console.log(unitInfo.halfway);
     return unitInfo;
@@ -25,54 +26,62 @@ if (Meteor.isClient) {
   Template.select.helpers({
     'unit': function() {
       return Units.find();
+    },
+    randomTip: function() {
+      tips = [
+        "This box is meant to show a random tip or message but I haven't written any yet."
+      ];
+      return tips[Math.floor((Math.random() * tips.length))];
     }
   });
 
   Template.display.helpers({
-    settings: function () {
+    settings: function() {
       return {
-        collection: Activities.find(Session.get('unit') ? {Unit: Session.get('unit')} : {}),
+        collection: Activities.find(Session.get('unit') ? { Unit: Session.get('unit') } : {}),
         class: 'table table-condensed table-responsive table-hover',
         rowsPerPage: 600,
         showRowCount: false,
         showNavigation: 'never',
         rowClass: colorFunction,
         filters: ['activity', 'theme', 'discipline', 'search', 'future', 'mst'],
-        fields: [
-          { key: 'Unit', label: 'Unit', sortOrder: (Session.get('unit') ? 1 : 0), hidden: function() {return Session.get('unit')}},
-          { key: 'Activity', label: 'Activity', fn: function(val, obj) { return val.split(' ')[0]}},
-          { key: 'Theme', label: 'Theme'},
-          { key: 'Series', label: 'P#'},
-          { key: 'Date', label: 'Date', sortOrder: (Session.get('unit') ? 0 : 1), sortByValue: true, fn: function(val, obj) {
-             if (Session.get('timeSet')) {
-               return moment(val).from(new Date());
-            } else {
-             return moment(val).format('h\u00a0a[,]\u00a0D\u00a0MMM');
+        fields: [{
+            key: 'Unit', label: 'Unit',
+            sortOrder: (Session.get('unit') ? 1 : 0),
+            hidden: function() {
+              return Session.get('unit')
             }
-          }},
-          { key: 'Title', label: 'Title'},
-          { key: 'Discipline', label: 'Discipline'},
-          { key: 'Subdiscipline', label: 'Subdiscpline'}
-          
+          }, {
+            key: 'Activity', label: 'Activity',
+            fn: function(val, obj) {
+              return val.split(' ')[0]
+            }
+          }, {
+            key: 'Theme', label: 'Theme'
+          }, {
+            key: 'Series', label: 'P#'
+          }, {
+            key: 'Date', label: 'Date',
+            sortOrder: (Session.get('unit') ? 0 : 1),
+            sortByValue: true,
+            fn: function(val, obj) {
+              if (Session.get('timeSet')) {
+                return moment(val).from(new Date());
+              } else {
+                return moment(val).format(
+                  'h\u00a0a[,]\u00a0D\u00a0MMM');
+              }
+            }
+          }, {
+            key: 'Title', label: 'Title'
+          }, {
+            key: 'Discipline', label: 'Discipline'
+          }, {
+            key: 'Subdiscipline', label: 'Subdiscpline'
+          }
+
         ]
       };
     }
-  });
-  Template.usageTip.helpers({
-    randomTip: function() {
-       tips = ["This box is meant to show a random tip or message but I haven't written any yet."];
-       return tips[Math.floor((Math.random() * tips.length))];
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-  /* Test data
-  Units.insert({number: 1011, class:'primary'});
-  Units.insert({number: 1022, class:'info'});
-  Units.insert({number: 2031, class:'success'});
-  Units.insert({number: 2042, class:'warning'});
-  */
   });
 }
